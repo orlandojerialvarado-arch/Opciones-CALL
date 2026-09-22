3243d15899e756c8339b409ec95d16d945a5331f
OrlandoJeri <orlando.jeri.alvarado@gmail.com>
2026-09-22 02:48:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
-st.write("Hola mundo")
+import streamlit as st
 
-st.file_uploader
+st.title("Prueba de Servidor Python Activo 🚀")
+st.write("¡Hola mundo! El motor de Python en la nube está corriendo.")
+
+archivo = st.file_uploader("Sube una prueba de archivo CSV aquí", type=["csv"])
+if archivo is not None:
+    st.success(f"Archivo recibido exitosamente: {archivo.name}")
